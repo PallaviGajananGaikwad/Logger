16c2bccae6c99eef1febc145cb5c352336109e29
Gargi <pallavi.gaikwad0399@gmail.com>
2024-04-11 13:33:21 +0530

    Commit

--- src/main/java/org/example/example/Console.java
+++ src/main/java/org/example/example/Console.java
@@ -12,20 +12,13 @@ public class Console
 
   //step3 3.create slf4j logger object
    private static Logger Log = LoggerFactory.getLogger(Console.class);
+
     public static void main(String[] args)throws IOException {
        //Step 1. Create layout
-     // Layout lay = new HTMLLayout();
-    Layout lay = new SimpleLayout();
- // Layout lay = new XMLLayout();
-      //  Layout lay = new PatternLayout("%p %d %c %M %m %n");
-      // %p= Priority method ||%d= date & time ||%c= class name
-        //%M= method name|| %m=message|| %n= nextline
-
+        Layout lay = new SimpleLayout();
         //Step 2. Create Appender + link layout
-     Appender app = new ConsoleAppender(lay);
-       // Appender app = new FileAppender(lay,"E:/Mylogs/data.log");
-
-        //step 4. Link appender with logger
+          Appender app = new ConsoleAppender(lay);
+         //step 4. Link appender with logger
          //Print messages
         Log.trace("From Trace");
         Log.debug("From Debug");
